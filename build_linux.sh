d05726b4878347d8e00db542f673f264f81d596c
Ritchie Swann <ritchieswann@gmail.com>
2024-05-21 12:47:46 +0100

    Build fixes
    Fix paths for linux build
    Use proper boost libs from CMake defs

--- build_linux.sh
+++ build_linux.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 cd build
-cmake ..
+cmake -DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES=$HOMEBREW_PREFIX/include -DCMAKE_CXX_STANDARD_LIBRARIES=-L$HOMEBREW_PREFIX/lib -DImageMagick_INCLUDE_DIRS=/usr/local/include/ImageMagick-7 ..
 make
