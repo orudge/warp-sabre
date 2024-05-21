bd4c8490eb89880baa02df1258c87c4b618382ce
Ritchie Swann <ritchieswann@gmail.com>
2024-05-21 12:02:28 +0100

    Make the build directory if it doesn't exists

--- build_homebrew.sh
+++ build_homebrew.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p build
 cd build
 cmake -DCMAKE_CXX_STANDARD_INCLUDE_DIRECTORIES=$HOMEBREW_PREFIX/include -DCMAKE_CXX_STANDARD_LIBRARIES=-L$HOMEBREW_PREFIX/lib ..
 make
--- build_mingw.sh
+++ build_mingw.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p build
 cd build
 cmake -DLIBXML2_INCLUDE_DIR=$MINGW_PREFIX/include/libxml2 -DImageMagick_INCLUDE_DIRS=$MINGW_PREFIX/include/ImageMagick-7 ..
 make
